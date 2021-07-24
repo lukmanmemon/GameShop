bc37a142a389774caea681ae9b2e82f0fb5b311d
lukmanmemon <memon_lukman@hotmail.com>
2021-07-23 22:20:26 -0400

    Travis up

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import empty_image from '../media/empty-cart-image.png';
 
-
 export class Cart extends React.Component {
     render() {
         return(
             <div id="cart-section">
                 <img src={empty_image} alt="empty cart" className="cart-content"></img>
             </div>
-
-
         );
     }
 }
